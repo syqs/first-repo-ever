276182d4b94ad95563e4b44c9408382a4eff40aa
Kristian Magda <kristian.magda@gmail.com>
2016-04-15 18:53:09 -0500

    some change again

--- js/leScript.js
+++ js/leScript.js
@@ -16,7 +16,7 @@ $.getJSON('http://hipsterjesus.com/api/', function(data) {
           });
 
 //Expand the textarea on clicking on it
-$(textarea).focus(function()
+$('textarea').focus(function()
               {
                  $(this).animate({'height': '185px'}, 'slow' );
                  return false;
