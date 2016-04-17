86f32db2e14bb499a957ae0db098e81fd3f885e4
Kristian Magda <kristian.magda@gmail.com>
2016-04-17 11:58:35 -0500

    added a bunch of comments

--- js/leScript.js
+++ js/leScript.js
@@ -1,89 +1,133 @@
-var mySonJ = {
-  'active link' : 'home'  ,
-  'option1' :  1,
-  'option3' : "waiting for JSON... it doesent seem to be arriving at this location for reasons we may never truly understand",
-  'description' : 'in my next key-value pair i will add a whole bunch of html that has been escaped for JavaScript code by a free online tool',
-  'form' : ' \r\n          <h3>Please fill out this form and feel free to skip parts you dont like. Just a few are * <\/h3>\r\n\r\n          <form action=\"https:\/\/formspree.io\/kristian.magda@gmail.com\" method=\"POST\" target=\"_blank\">\r\n          <div class=\"form-group\">\r\n            <label for=\"Email1\">Email address<\/label>\r\n            <input type=\"email\" name=\"email\"class=\"form-control\" id=\"Email1\" placeholder=\"Email\"  >\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"name\">Name *<\/label>\r\n            <input type=\"name\" name=\"fullName\" class=\"form-control\" id=\"name1\" placeholder=\"Name\" required=\"required\"  >\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"name\">A short bio about you *<\/label>\r\n            <input type=\"textarea\" name=\"shortBios\"class=\"form-control big\" id=\"shortBio\" required=\"required\" >\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"backgrounds\">Your programing background<\/label>\r\n            <input type=\"textarea\" name=\"backs\" class=\"form-control big\" id=\"backgrounds\">\r\n\r\n            <!--the formspree service spam fightbot -->\r\n            <input type=\"text\" name=\"_gotcha\" style=\"display:none\" \/>\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"reasons1\">What is the prize you seek?<\/label>\r\n            <input type=\"textarea\" name=\"reason\"  class=\"form-control big\" id=\"reasons1\">\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"goals\">Goals and aspirations * <\/label>\r\n            <input type=\"textarea\" name=\"goal\" class=\"form-control big\" id=\"goals\" required=\"required\" >\r\n            <a href=\"#EasterEggs4All\" onclick=\"easterEgg()\">><\/a>\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"hobbies\">What do you do when you are not doing the things you do?<\/label>\r\n            <input type=\"textarea\" name=\"hobbie\"class=\"form-control big\" id=\"hobbies\">\r\n          <\/div>\r\n          <div class=\"form-group\">\r\n            <label for=\"favs\">Your web inspirations and fav-bookmarks<\/label>\r\n            <input type=\"textarea\" name=\"fav\"class=\"form-control big\" id=\"favs\">\r\n          <\/div>\r\n          <div class=\"checkbox\">\r\n            <label for=\"checks\">Check me if thy dare<\/label>\r\n              <input type=\"checkbox\" name\"checkboxer\" id=\"checks\">\r\n          <\/div>\r\n          <button type=\"submit\" class=\"btn btn-default\">Submit<\/button>\r\n        <\/form>',
-  'what' : '        <p>The font \'Kaushan Script\' used with google font api<\/p>\r\n        <a href=\"https:\/\/www.google.com\/fonts\" target=\"_blank\">https:\/\/fonts.googleapis.com\/css?family=Kaushan+Script\'<\/a>\r\n        <p>jQuery and Bootstrap have been loaded<\/p>\r\n        <a href=\"http:\/\/getbootstrap.com\/\" target=\"_blank\">Bootstrap<\/a>\r\n        <p>Couple of examples of Javascript functions<\/p>\r\n        <p>Content loaded from api stored in JS object<\/p>'
-}
-$.getJSON('http://hipsterjesus.com/api/', function(data) {
-      /* page-test is the class of the html element that gets populated with JSON data */
-      mySonJ.option4 = data.text;
-  });
+// short intro... this text almost contains more comments than code this is soley for my own practice and understanding, ur welcome to learn from it too tho.
 
+// function to connect to hipsterjesus and request some data then
+// assign that data to the object mySonJ at key 'option4'
 
-function setTitle(title){
-  if (mySonJ['active link']==='who'){
-    $('.title').html('<h1>Hipster Ipsum<\/h1>\n<h3>try clicking on the \'firstWord()\' function</h3>');
-  }else if(mySonJ['active link']=='home'){
-      $('.title').html('<h1>Questions for classmates<\/h1>');
-  }else if(mySonJ['active link']=='what'){
-      $('.title').html('<h1>References<\/h1>');
-  }else if(mySonJ['active link']=='whom'){
-      $('.title').html('<h1>Hipster Ipsum<\/h1>\n<h3 class=\'red\'>Click \'the Who\' link to generate new hipster ipsum</h3>');
-  }else{};
+function getIpsum() {
+    $.getJSON('http://hipsterjesus.com/api/', function(data) {
+          mySonJ.option4 = data.text;
+    });
 };
+
+//Invoke the function
+getIpsum();
+
+// Function sets title for the page by going through an if else loop that adds html code to the title class elements
+// it chooses what to add based on the current value of the 'active link' key-value pair from the mySonJ object
+
+function setTitle(objectKey){
+    if (mySonJ['active link']==='who'){
+      $('.title').html('<h1>Hipster Ipsum<\/h1>\n<h3>try clicking on the \'firstWord()\' function</h3>');
+    }else if(mySonJ['active link']==='home'){
+      $('.title').html('<h1>Questions for classmates<\/h1>');
+    }else if(mySonJ['active link']==='what'){
+      $('.title').html('<h1>References<\/h1>');
+    }else if(mySonJ['active link']==='whom'){
+      $('.title').html('<h1>Hipster Ipsum<\/h1>\n<h3 class=\'red\'>Click \'the Who\' link to generate new hipster ipsum</h3>');
+    }else{};
+};
+
+// next 3 functions do the setting and executing part
 function home (){
+
+  // when home function is invoked first set object's key 'active link' to have the value of 'home'
+  // this is done in order to keep track of the selectet link in the main menu with Javascript
   mySonJ['active link'] = "home";
-  setTitle('home');
+
+  //Invoke the setTitle function to set the html data based on the current value of the key 'active link'
+  setTitle();
+
+  //Call upont the powers of jQuery to add html data form the mySonJ object to the page element with class '.page'
   $('.page').html(mySonJ.form);
+
+  // Invoke the expand function in this exectution context (expands the textarea of the html elements in height via jQuery)
   expand();
 };
+
+// same as home
 function what (){
   mySonJ['active link'] = "what";
-  setTitle('what');
+  setTitle();
   $('.page').html(mySonJ.what);
 };
+
 function what2 (){
   mySonJ['active link'] = "whom";
-  setTitle('whom');
+  setTitle();
   $('.page').html(mySonJ.whom);
 };
-function abouts(n){
-  if (n == 1) {
+
+// function generates two different results based on navigation context user is curently in stored and retrived from mySonJ object
+function who(){
+  if (mySonJ.option1 == 1) {
     mySonJ['active link'] = "who";
-    setTitle('who');
+    setTitle();
     mySonJ.option1 = 2;
-  }else if(n==2){
-    setTitle('whom');
+  }else if(mySonJ.option1==2){
+    mySonJ['active link'] = "whom";
+    setTitle();
+    mySonJ.option1 = 1;
   }
+  // load some text from object in case u are not getting a response from the next invocation of getIpsum
   $('.page').html(mySonJ.option3);
-  /* hipster version of lorem ipsum text generator with api for dynamic embeding */
-  $.getJSON('http://hipsterjesus.com/api/', function(data) {
-        /* page-test is the class of the html element that gets populated with JSON data */
-        mySonJ.option4 = data.text;
-    });
-    $('.page').html(mySonJ.option4);
+  // call upon the hipster ipsum api and store the result in the object
+  getIpsum();
+  // set html of all the DOM elements with '.page' class attribute
+  $('.page').html(mySonJ.option4);
 };
 
 
 function expand(){
-  //Expand the textarea by clicking on it
+  //Expand the input textarea by clicking on it using jquery 'focus' function and passing a new function as a parametar for 'focus'
   $('input').focus(function()
     {
+      // jquery function that animates the DOM element by passing an object with css values asigned to key value pairs
+      // and a parametar 'slow' that sets the time of the animation exectution
        $(this).animate({'height': '100px'}, 'slow' );
        return false;
      });
 };
 
+// function to output values from internal exectution stacks in the global exectution stack
+function logOutput(a) {
+    console.log(a);
+};
+
 // Function that encapsulates first word of the any link on the page
-// with a span element using jQuery (also adds a first-word class to the element)
+// in a span element using jQuery (also adds a first-word class to that element)
 function firstWord(selector, size) {
 
+// Depending on the parametar passed to the function it will set all of the specified elemtens ie. <a><p><li> ..
+// to have the first word replaced using jquery's replace function and passing it a value
   if (size == "small") {
   $(selector).each(function(){
+
+    // create a jquery object and store it in the variable word
       var word = $(this);
+
+      // use two functions with 'word' object. First call the function 'html' to add html to the elemnt,
+      // pass it the 'replace' function as the parametar to the 'text' function and then call function 'html' with result as a parametar
+      // $1 returns the copy of first word that is being replaced while nesting the word  in a <span> element
+
+      //more on how this can be done checkout out this jquery plugin http://benalman.com/code/projects/jquery-replacetext/examples/replacetext/
       word.html( word.text().replace(/(^\w+)/,'<span class=\"first-word small\">$1</span>') );
+
     });
+
+    // same concept different class being appended to the html element
   }else if (size == "big") {
     $(selector).each(function(){
         var word = $(this);
+        // logOutput(word);
         word.html( word.text().replace(/(^\w+)/,'<span class=\"first-word big\">$1</span>') );
       });
-    what2();
+
+    // if we are curently on 'about page*(the Who)' call what2() otherwise do nothing
+    if (mySonJ['active link'] == 'who'){ what2();}else{};
   }
 };
 
+// invoke some functions
 firstWord("a", 'small');
 firstWord("p", 'big');
 home();
